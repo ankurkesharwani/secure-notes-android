ae65118ec5944b4e1d95c7302184dae6e19369bd
Ankur Kesharwani <ankur.kesharwani001@gmail.com>
2021-09-08 21:32:01 +0530

    Password viewer - Work in progress

--- app/src/main/java/com/ankur/securenotes/ui/fragment/password/viewer/PasswordViewerFragment.kt
+++ app/src/main/java/com/ankur/securenotes/ui/fragment/password/viewer/PasswordViewerFragment.kt
@@ -1,19 +1,131 @@
 package com.ankur.securenotes.ui.fragment.password.viewer
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ankur.securenotes.databinding.FragmentPasswordListBinding
 import com.ankur.securenotes.databinding.FragmentPasswordViewerBinding
+import com.ankur.securenotes.shared.Shared
+import com.ankur.securenotes.task.GetPasswordByIdTask
+import com.ankur.securenotes.taskexecuter.SerialTaskExecutor
+import com.ankur.securenotes.taskexecuter.Task
+import com.ankur.securenotes.ui.common.adapter.ListViewableRecyclerViewAdapter
+import com.ankur.securenotes.ui.common.adapter.PasswordListRecycleViewAdapter
+import com.ankur.securenotes.ui.fragment.password.editor.PasswordEditorFragment
+import com.ankur.securenotes.ui.fragment.password.editor.PasswordEditorFragmentManager
+import com.ankur.securenotes.ui.fragment.password.editor.PasswordEditorFragmentManagerBuilder
+import com.ankur.securenotes.ui.fragment.password.list.PasswordListFragment
+import com.ankur.securenotes.ui.fragment.password.list.PasswordListFragmentManager
+import com.ankur.securenotes.ui.fragment.password.list.PasswordListFragmentManagerBuilder
 
-class PasswordViewerFragment : Fragment() {
+class PasswordViewerFragment : Fragment(), SerialTaskExecutor.Listener {
 
   private lateinit var binding: FragmentPasswordViewerBinding
+  private lateinit var activity: Activity
+  private lateinit var adapter: ListViewableRecyclerViewAdapter
+  private lateinit var manager: PasswordViewerFragmentManager
+
+  private var passwordId: String? = null
+
+  override fun onAttach(context: Context) {
+    super.onAttach(context)
+
+    activity = context as Activity
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    setArgs()
+    setDependencies(savedInstanceState)
+  }
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
     binding = FragmentPasswordViewerBinding.inflate(layoutInflater, container, false)
-    return binding.root
+    val view = binding.root
+
+    setupRecyclerView()
+
+    return view
+  }
+
+  override fun onStart() {
+    super.onStart()
+
+    fetchPassword()
+  }
+
+  override fun onResume() {
+    super.onResume()
+
+    reloadData()
+  }
+
+  override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+
+    if (activity.isChangingConfigurations) {
+      outState.putBoolean("isChangingConfiguration", true)
+      Shared.store!!.save(TAG, "manager", manager)
+    } else {
+      outState.putBoolean("isChangingConfiguration", false)
+    }
+  }
+
+  private fun setupRecyclerView() {
+    val linearLayoutManager = LinearLayoutManager(activity)
+    binding.recyclerView.layoutManager = linearLayoutManager
+    adapter = ListViewableRecyclerViewAdapter(null)
+    binding.recyclerView.adapter = adapter
+  }
+
+  private fun setArgs() {
+    passwordId = arguments?.get(PasswordEditorFragment.PARAM_PASSWORD_ID) as? String
+  }
+
+  private fun setDependencies(savedInstanceState: Bundle?) {
+    manager = if (savedInstanceState?.getBoolean("isChangingConfiguration") == true) {
+      Shared.store?.retrieve(TAG, "manager") as PasswordViewerFragmentManager
+    } else {
+      context?.let {
+        PasswordViewerFragmentManagerBuilder().set(context = activity).build()
+      }!!
+    }
+  }
+
+  private fun fetchPassword() {
+    this.passwordId?.let {
+      val getPasswordTask = GetPasswordByIdTask(it, Shared.getReadableDatabase(activity))
+      Shared.serialTaskExecutor?.exec(getPasswordTask, this)
+    }
+  }
+
+  @SuppressLint("NotifyDataSetChanged")
+  private fun reloadData() {
+    //adapter.updatePasswords(manager.passwords)
+    //adapter.notifyDataSetChanged()
+  }
+
+  override fun onTaskStarted(task: Task) {
+
+  }
+
+  override fun onTaskFinished(task: Task) {
+    when (task) {
+      is GetPasswordByIdTask -> {
+        task.result?.password?.let {
+          manager.password = it
+
+          reloadData()
+        }
+      }
+    }
   }
 
   companion object {
--- app/src/main/java/com/ankur/securenotes/ui/fragment/password/viewer/PasswordViewerFragmentManager.kt
+++ app/src/main/java/com/ankur/securenotes/ui/fragment/password/viewer/PasswordViewerFragmentManager.kt
@@ -1,9 +1,11 @@
 package com.ankur.securenotes.ui.fragment.password.viewer
 
 import android.content.Context
+import com.ankur.securenotes.entity.PasswordEntity
 
 interface PasswordViewerFragmentManager {
 
   var context: Context?
+  var password: PasswordEntity?
 
 }
